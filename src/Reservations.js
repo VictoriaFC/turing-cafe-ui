1ea5c518e86cde6116e826f0a24eaa184f769ab0
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-20 10:02:50 -0600

    Create ResyCard.js and ResyCard.css

--- src/Reservations.js
+++ src/Reservations.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Reservations.css'
 
 const Reservations = () => {
+
 	return(
 		<div className='reservations-container'>
 			<h1>Reservations Container</h1>
---
+++ src/ResyCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '/Users/victoriafox-collis/turing/frontend/mod3/turing-cafe-ui/src/ResyCard.css'
+
+const ResyCard = () => {
+	return(
+		<div className='resy-card'>Resy Card</div>
+	)
+}
+
+export default ResyCard
