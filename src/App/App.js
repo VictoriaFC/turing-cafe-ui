ddaa6c21063c54669ea14752904717e37945a9fc
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-20 09:43:58 -0600

    Add componentDidMount to fetch reservations

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor() {
+		super()
+		this.state = {
+			reservations: null
+		}
+	}
+
+	componentDidMount = () => {
+		fetch('http://localhost:3001/api/v1/reservations')
+			.then(response => response.json())
+			.then(reservations => this.setState({ reservations }))
+	}
+
+
+	
+	render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
