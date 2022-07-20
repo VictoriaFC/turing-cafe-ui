d54297329362564810cc5f6e56d594fc0929d8d4
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-20 10:16:26 -0600

    Fix fetch bug by changing reservations state from null to empty array

--- src/App/App.js
+++ src/App/App.js
@@ -6,30 +6,15 @@ class App extends Component {
   constructor() {
 		super()
 		this.state = {
-			reservations: [
-				{
-					"id": 1,
-					"name": "Christie",
-					"date": "12/29",
-					"time": "7:00",
-					"number": 12
-			},
-			{
-					"id": 2,
-					"name": "Leta",
-					"date": "4/5",
-					"time": "7:00",
-					"number": 2
-			}
-			]
+			reservations: []
 		}
 	}
 
-	// componentDidMount = () => {
-	// 	fetch('http://localhost:3001/api/v1/reservations')
-	// 		.then(response => response.json())
-	// 		.then(reservations => this.setState({ reservations }))
-	// }
+	componentDidMount = () => {
+		fetch('http://localhost:3001/api/v1/reservations')
+			.then(response => response.json())
+			.then(reservations => this.setState({ reservations }))
+	}
 	
 	render() {
     return (
