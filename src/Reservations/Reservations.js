b8dbbf4bef9a358a10775b27605faa395d34e9a8
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-08 11:46:51 -0600

    Pass props to Reservations component

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Reservations.css'
+import ResyCard from '../ResyCard/ResyCard'
 
 const Reservations = ({reservations}) => {
+	const resyCards = reservations.map(reservation => {
+		return(
+			<ResyCard 
+				id={reservation.id}
+				name={reservation.name}
+				date={reservation.date}
+				number={reservation.number}
+			/>
+		)
+	})
 	return (
-		<div className="reservations-container">I'm the reservatiosn container</div>
+		<div className="reservations-container">
+			{resyCards}
+		</div>
 	)
 }
 
